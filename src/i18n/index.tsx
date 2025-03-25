fb43d48e008d22b351ee9dda18e4137c5abaee83
Guilherme Trevizani <guilhermetrevizani@hotmail.com>
2025-03-25 23:50:04 +0000

    refactor: globalization

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -1,19 +1,25 @@
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationPortugueseBrazil from './languages/pt-BR.json';
+import translationEnglishUnitedStates from './languages/en-US.json';
 
-import translationPortugueBrazilian from "./ptbr.json";
+export const stringFormat = (str: string, ...args: any[]) =>
+  str.replace(/{(\d+)}/g, (match, index) => args[index] || '');
 
 const resources = {
-  ptbr: {
-    translation: translationPortugueBrazilian,
+  'pt-BR': {
+    translation: translationPortugueseBrazil,
   },
+  'en-US': {
+    translation: translationEnglishUnitedStates
+  }
 }
 
 i18next
   .use(initReactI18next)
   .init({
     resources,
-    lng: "ptbr",
+    lng: process.env.REACT_APP_LANGUAGE
   });
 
 export default i18next;
